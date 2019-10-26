fe10a4ae2baac7d6cc5f55c3e3c05f2ac32d47e3
denghongbo <denghongbo@ihengtech.com>
2019-10-26 09:49:25 +0800

    update

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=w1.php-x.com;dbname=ihengtech_adv',
-    'username' => 'test',
-    'password' => 'test_WSX3edc',
+    'dsn' => 'mysql:host=localhost;dbname=ihengtech_adv',
+    'username' => 'adv',
+    'password' => 'adv_WSX3edc',
     'charset' => 'utf8mb4',
     'tablePrefix' => 'adv_',
 
